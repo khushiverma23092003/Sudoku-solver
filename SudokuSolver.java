e891c9235d58582e7014edad665e85eec54c56c4
Khushi Verma <iamkhushiv239@gmail.com>
2025-07-30 12:51:12 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,38 @@
+public class SudokuSolver {
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) return true;
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col, int num) {
+        for (int x = 0; x < 9; x++) {
+            if (board[row][x] == num || board[x][col] == num ||
+                board[row - row % 3 + x / 3][col - col % 3 + x % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
